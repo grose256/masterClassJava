4d81d7a0df6528005fca846b0d7d54d848018ec7
grose256 <georgerose1613@gmail.com>
2021-12-16 17:24:53 -0500

    section 3 - first steps

---
+++ sect3FirstSteps/src/module-info.java
@@ -0,0 +1,2 @@
+module sect3FirstSteps {
+}
---
+++ sect3FirstSteps/src/sect3/Hello.java
@@ -0,0 +1,19 @@
+package sect3;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello George!");
+		
+		double myFirstNumber = (10 + 5) + (2 * 10);
+		System.out.println("My first number is : " + myFirstNumber);
+		
+		
+		
+		
+		
+
+	}
+
+}
